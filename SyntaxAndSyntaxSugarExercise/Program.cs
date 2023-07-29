793121ff0c1b0f1abbcb688bbeab432c71fdc951
Trigail Curtis <trigailcurtisabm@gmail.com>
2023-07-29 17:08:31 -0500

    done

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -8,3 +8,19 @@
         }
     }
 }
+//int answer = 4;  //explict typing
+//string response; //explict typing
+
+//if (answer < 9)
+//{
+//    response = answer + " is less than nine";
+//}
+//else
+//{
+//    response = answer + " is greater than or equal to nine";
+//}
+
+var answer = 4;
+var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+
+Console.WriteLine(response);
